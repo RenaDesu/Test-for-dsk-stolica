0802e66e6baf95cca50324c0064fb559cd4a250c
RenaDesu <rena-desu@mail.ru>
2023-02-08 18:01:54 +0300

    Добавляет верстку по макету-скрину 1я итерация

---
+++ index.js
@@ -0,0 +1,26 @@
+const productSliderLine = document.querySelector('.product-card__slider-line');
+const productSliderDots = document.querySelectorAll('.product-card__slider-dot');
+
+let sliderCount = 0;
+
+const rollSlider = () => {
+    let sliderWidth = document.querySelector('.product-card__slider').offsetWidth;
+    productSliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+const currentSlide = (index) => {
+    productSliderDots.forEach(item => item.classList.remove('product-card__slider-dot--active'));
+    productSliderDots[index].classList.add('product-card__slider-dot--active');
+}
+
+const showSlide = () => {
+    productSliderDots.forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+            sliderCount = index;
+            rollSlider();
+            currentSlide(sliderCount);
+        })
+    })
+}
+
+showSlide();
